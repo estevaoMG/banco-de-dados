7c00f590c2a7923d21b34b9c082edaab018fedd4
estevaoMG <estevao.gouveia@hotmail.com>
2025-12-29 15:15:05 -0300

    Inserindo registros

--- conexao.py
+++ conexao.py
@@ -3,6 +3,10 @@ import sqlite3
 conexao = sqlite3.connect("meu_banco.db")
 cursor = conexao.cursor()
 
-cursor.execute(
-    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
-)
+# cursor.execute(
+#     "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+# )
+
+data = ("Giovanna", "gi@gmail.com")
+cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?);", data)
+conexao.commit()
