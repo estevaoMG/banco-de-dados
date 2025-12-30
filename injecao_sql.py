f93eadf176ff7879d7f1599bbc0353b3affb16a3
estevaoMG <estevao.gouveia@hotmail.com>
2025-12-30 18:02:22 -0300

    Boas práticas - Código inseguro que permite injeção SQL

---
+++ injecao_sql.py
@@ -0,0 +1,15 @@
+# Código inseguro que permite injeção SQL
+
+import sqlite3
+
+conexao = sqlite3.connect("meu_banco.db")
+cursor = conexao.cursor()
+cursor.row_factory = sqlite3.Row
+
+id_cliente = input("Informe o id do cliente: ")
+cursor.execute(f"SELECT * FROM clientes WHERE id={id_cliente}")
+
+clientes = cursor.fetchall()
+
+for cliente in clientes:
+    print(dict(cliente))
